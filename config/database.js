db38db1c366c308e9ec3001dc6464c4c1b792351
Dea <deaaa.putri.1995@gmail.com>
2025-11-28 14:53:52 +0800

    fix port, base url, and mysql env

--- config/database.js
+++ config/database.js
@@ -8,12 +8,13 @@ const pool = mysql.createPool({
     port: process.env.MYSQLPORT,
     waitForConnections: true,
     connectionLimit: 20,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: { rejectUnauthorized: false }
 });
 
 pool.getConnection()
     .then(conn => {
-        console.log('✅ DATABASE CONNECTED');
+        console.log('✅ DATABASE CONNECTED TO RAILWAY');
         conn.release();
     })
     .catch(err => {
